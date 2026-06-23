b1ffe950f48184f0fd68413015fa05b5fc8165d7
HeoTaehoon7 <greenclass.hoon@gmail.com>
2026-06-23 16:37:35 +0900

    Member 관리자회원추가

--- src/main/java/com/example/springedu2/controller/AdminController.java
+++ src/main/java/com/example/springedu2/controller/AdminController.java
@@ -3,10 +3,14 @@ package com.example.springedu2.controller;
 import com.example.springedu2.dto.MemberCreateForm;
 import com.example.springedu2.entity.Member;
 import com.example.springedu2.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -27,13 +31,29 @@ public class AdminController {
 
     }
 
-    //회원 추가
+    //회원 추가(관리자가)
     @PostMapping("/admin/members")
-    public String addCreate(MemberCreateForm memberCreateForm) {
+    @Transactional
+    public String addCreate(
+            @Valid @ModelAttribute("memberForm") MemberCreateForm memberCreateForm,
+            BindingResult bindingResult
+            ) {
+        if(bindingResult.hasErrors()){
+            return "memberAdminForm";  // 다시 입력받아라
+        }
 
+        // 새 회원 추가 관리자가
+        try {
+            memberService.create(memberCreateForm);
+        } catch (IllegalArgumentException e) {
+            bindingResult.reject("createFail", e.getMessage());
+            return "memberAdminForm";  // 회원추가 실패 -> 다시 추가화면으로 이동
+        }
+
+        return "redirect:/admin/members"; // 목록조회
     }
 
-    // 회원추가
+    // 회원추가를 위해 입력받는 화면
     @GetMapping("/admin/members/new")
     public String adminCreateForm(Model model) {
         model.addAttribute("memberForm", new MemberCreateForm());
